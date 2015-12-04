ee83e71a806e05a6502bf9ed77c8839e4380913e
anthony-zhu <abzhu@ucla.edu>
2015-12-04 19:48:51 +0000

    Alternative bluetooth setup (more stable)

--- Edison/start-script.sh
+++ Edison/start-script.sh
@@ -4,14 +4,18 @@
 /home/root/SmartHelmet/Edison/run-python.sh &
 rfkill unblock bluetooth
 
+# This method is very touchy!
 # Pair to Nexus 7 (ID 50:46:5D:77:25:1C)
-echo -e "power on\n" | bluetoothctl
-sleep 2
-echo -e "scan on\n" | bluetoothctl
-sleep 2
-echo -e "pair 50:46:5D:77:25:1C\n" | bluetoothctl
-sleep 2
-echo -e "discoverable on\n" | bluetoothctl
-sleep 1
-echo -e "trust 50:46:5D:77:25:1C\n" | bluetoothctl
-sleep 2
+#echo -e "power on\n" | bluetoothctl
+#sleep 2
+#echo -e "scan on\n" | bluetoothctl
+#sleep 2
+#echo -e "pair 50:46:5D:77:25:1C\n" | bluetoothctl
+#sleep 2
+#echo -e "discoverable on\n" | bluetoothctl
+#sleep 1
+#echo -e "trust 50:46:5D:77:25:1C\n" | bluetoothctl
+#sleep 2
+
+# Alternative pairing
+bluetoothctl < /home/root/SmartHelmet/Edison/file.txt
